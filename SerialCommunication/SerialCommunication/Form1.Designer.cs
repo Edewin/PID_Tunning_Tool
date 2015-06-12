882ddce87c59b9101471a8fa63949da04a18357d
Edewin <eduard.soltuzu@outlook.com>
2015-06-12 16:38:44 +0300

    first Version (untested on real device)

---
+++ SerialCommunication/SerialCommunication/Form1.Designer.cs
@@ -0,0 +1,389 @@
+﻿namespace SerialCommunication
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.setPoint = new System.Windows.Forms.NumericUpDown();
+            this.checkPause = new System.Windows.Forms.CheckBox();
+            this.buttonClearText = new System.Windows.Forms.Button();
+            this.buttonWrite = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.buttonDisconnect = new System.Windows.Forms.Button();
+            this.buttonConnect = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.buttonWriteConfigurations = new System.Windows.Forms.Button();
+            this.KdValue = new System.Windows.Forms.NumericUpDown();
+            this.KiValue = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.KpValue = new System.Windows.Forms.NumericUpDown();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.setPoint)).BeginInit();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.KdValue)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.KiValue)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.KpValue)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.setPoint);
+            this.groupBox1.Controls.Add(this.checkPause);
+            this.groupBox1.Controls.Add(this.buttonClearText);
+            this.groupBox1.Controls.Add(this.buttonWrite);
+            this.groupBox1.Location = new System.Drawing.Point(28, 134);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(198, 111);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Write SetPoint";
+            // 
+            // setPoint
+            // 
+            this.setPoint.DecimalPlaces = 4;
+            this.setPoint.Increment = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.setPoint.Location = new System.Drawing.Point(6, 19);
+            this.setPoint.Maximum = new decimal(new int[] {
+            50000,
+            0,
+            0,
+            0});
+            this.setPoint.Name = "setPoint";
+            this.setPoint.Size = new System.Drawing.Size(185, 20);
+            this.setPoint.TabIndex = 5;
+            // 
+            // checkPause
+            // 
+            this.checkPause.Appearance = System.Windows.Forms.Appearance.Button;
+            this.checkPause.AutoSize = true;
+            this.checkPause.BackColor = System.Drawing.Color.Lime;
+            this.checkPause.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkPause.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.checkPause.Location = new System.Drawing.Point(64, 72);
+            this.checkPause.Name = "checkPause";
+            this.checkPause.Size = new System.Drawing.Size(69, 30);
+            this.checkPause.TabIndex = 4;
+            this.checkPause.Text = "Pause";
+            this.checkPause.UseVisualStyleBackColor = false;
+            this.checkPause.CheckedChanged += new System.EventHandler(this.checkPause_CheckedChanged);
+            // 
+            // buttonClearText
+            // 
+            this.buttonClearText.Location = new System.Drawing.Point(117, 46);
+            this.buttonClearText.Name = "buttonClearText";
+            this.buttonClearText.Size = new System.Drawing.Size(75, 23);
+            this.buttonClearText.TabIndex = 3;
+            this.buttonClearText.Text = "Clear";
+            this.buttonClearText.UseVisualStyleBackColor = true;
+            this.buttonClearText.Click += new System.EventHandler(this.buttonClearText_Click);
+            // 
+            // buttonWrite
+            // 
+            this.buttonWrite.Location = new System.Drawing.Point(6, 46);
+            this.buttonWrite.Name = "buttonWrite";
+            this.buttonWrite.Size = new System.Drawing.Size(75, 23);
+            this.buttonWrite.TabIndex = 2;
+            this.buttonWrite.Text = "Write";
+            this.buttonWrite.UseVisualStyleBackColor = true;
+            this.buttonWrite.Click += new System.EventHandler(this.buttonWrite_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.BackColor = System.Drawing.SystemColors.Menu;
+            this.groupBox3.Controls.Add(this.comboBox2);
+            this.groupBox3.Controls.Add(this.buttonDisconnect);
+            this.groupBox3.Controls.Add(this.buttonConnect);
+            this.groupBox3.Controls.Add(this.comboBox1);
+            this.groupBox3.Location = new System.Drawing.Point(28, 19);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(197, 92);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Settings";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "9600",
+            "115200"});
+            this.comboBox2.Location = new System.Drawing.Point(87, 19);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(104, 21);
+            this.comboBox2.TabIndex = 3;
+            this.comboBox2.Text = "Select Baudrate";
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // buttonDisconnect
+            // 
+            this.buttonDisconnect.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonDisconnect.Location = new System.Drawing.Point(87, 54);
+            this.buttonDisconnect.Name = "buttonDisconnect";
+            this.buttonDisconnect.Size = new System.Drawing.Size(104, 32);
+            this.buttonDisconnect.TabIndex = 2;
+            this.buttonDisconnect.Text = "Disconnect";
+            this.buttonDisconnect.UseVisualStyleBackColor = true;
+            this.buttonDisconnect.Click += new System.EventHandler(this.buttonDisconnect_Click);
+            // 
+            // buttonConnect
+            // 
+            this.buttonConnect.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonConnect.Location = new System.Drawing.Point(6, 54);
+            this.buttonConnect.Name = "buttonConnect";
+            this.buttonConnect.Size = new System.Drawing.Size(75, 32);
+            this.buttonConnect.TabIndex = 1;
+            this.buttonConnect.Text = "Connect";
+            this.buttonConnect.UseVisualStyleBackColor = true;
+            this.buttonConnect.Click += new System.EventHandler(this.buttonConnect_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(6, 19);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(75, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.Text = "Select Port";
+            this.comboBox1.DropDown += new System.EventHandler(this.comboBox1_DropDown);
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(28, 378);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(198, 217);
+            this.textBox1.TabIndex = 3;
+            // 
+            // chart1
+            // 
+            chartArea1.BorderWidth = 3;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            this.chart1.Dock = System.Windows.Forms.DockStyle.Fill;
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(264, 0);
+            this.chart1.Name = "chart1";
+            this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Fire;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.StepLine;
+            series1.Legend = "Legend1";
+            series1.Name = "SetPoint";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series2.CustomProperties = "LineTension=0.1";
+            series2.Legend = "Legend1";
+            series2.Name = "RealValue";
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(784, 601);
+            this.chart1.TabIndex = 4;
+            this.chart1.Text = "chart1";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.groupBox2.Controls.Add(this.groupBox4);
+            this.groupBox2.Controls.Add(this.groupBox3);
+            this.groupBox2.Controls.Add(this.groupBox1);
+            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.groupBox2.Location = new System.Drawing.Point(0, 0);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(264, 601);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.buttonWriteConfigurations);
+            this.groupBox4.Controls.Add(this.KdValue);
+            this.groupBox4.Controls.Add(this.KiValue);
+            this.groupBox4.Controls.Add(this.label3);
+            this.groupBox4.Controls.Add(this.label2);
+            this.groupBox4.Controls.Add(this.label1);
+            this.groupBox4.Controls.Add(this.KpValue);
+            this.groupBox4.Location = new System.Drawing.Point(28, 252);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(197, 120);
+            this.groupBox4.TabIndex = 4;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "PID";
+            // 
+            // buttonWriteConfigurations
+            // 
+            this.buttonWriteConfigurations.Location = new System.Drawing.Point(30, 91);
+            this.buttonWriteConfigurations.Name = "buttonWriteConfigurations";
+            this.buttonWriteConfigurations.Size = new System.Drawing.Size(141, 23);
+            this.buttonWriteConfigurations.TabIndex = 6;
+            this.buttonWriteConfigurations.Text = "Write Configurations";
+            this.buttonWriteConfigurations.UseVisualStyleBackColor = true;
+            // 
+            // KdValue
+            // 
+            this.KdValue.DecimalPlaces = 3;
+            this.KdValue.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            131072});
+            this.KdValue.Location = new System.Drawing.Point(64, 65);
+            this.KdValue.Name = "KdValue";
+            this.KdValue.Size = new System.Drawing.Size(82, 20);
+            this.KdValue.TabIndex = 5;
+            // 
+            // KiValue
+            // 
+            this.KiValue.DecimalPlaces = 2;
+            this.KiValue.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.KiValue.Location = new System.Drawing.Point(64, 39);
+            this.KiValue.Name = "KiValue";
+            this.KiValue.Size = new System.Drawing.Size(82, 20);
+            this.KiValue.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(40, 67);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(23, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Kd:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(39, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(19, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Ki:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Kp:";
+            // 
+            // KpValue
+            // 
+            this.KpValue.DecimalPlaces = 1;
+            this.KpValue.Location = new System.Drawing.Point(64, 9);
+            this.KpValue.Name = "KpValue";
+            this.KpValue.Size = new System.Drawing.Size(82, 20);
+            this.KpValue.TabIndex = 0;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Menu;
+            this.ClientSize = new System.Drawing.Size(1048, 601);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.groupBox2);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "PID Tunning";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.setPoint)).EndInit();
+            this.groupBox3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.KdValue)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.KiValue)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.KpValue)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button buttonWrite;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button buttonDisconnect;
+        private System.Windows.Forms.Button buttonConnect;
+        private System.Windows.Forms.Button buttonClearText;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.CheckBox checkPause;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Button buttonWriteConfigurations;
+        private System.Windows.Forms.NumericUpDown KdValue;
+        private System.Windows.Forms.NumericUpDown KiValue;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown KpValue;
+        private System.Windows.Forms.NumericUpDown setPoint;
+    }
+}
+
---
+++ SerialCommunication/SerialCommunication/Form1.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace SerialCommunication
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+            chart1.Series[0].BorderWidth = 3;
+            chart1.Series[1].BorderWidth = 3;
+        }
+
+
+        #region Received Data from Serial Port
+
+
+        #region Variables
+
+        // buffer variable
+        private string bufferSerial; 
+        
+        // Counter for X-axis
+        private static double xCounter = 0;
+        #endregion
+
+
+        #region Received Data 
+
+
+        private void serialPort1_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            bufferSerial = serialPort1.ReadExisting();
+            this.Invoke(new EventHandler(displayText));
+        }
+
+
+        private void displayText(object sender, EventArgs e) 
+        {
+            if (checkPause.Checked)
+            {
+                textBox1.AppendText(Environment.NewLine + "RX:" + bufferSerial);
+            }
+            else
+            {
+                chart1.Series[0].Points.AddXY(xCounter, Convert.ToDouble(Convert.ToString(setPoint.Value)));
+                chart1.Series[1].Points.AddXY(xCounter, Convert.ToDouble(bufferSerial));
+                xCounter+=0.1;
+                textBox1.AppendText(Environment.NewLine + "RX:" + bufferSerial);
+            }
+           
+        }
+
+        #endregion 
+
+
+        #endregion
+
+
+        #region Get Available Ports
+
+        private void comboBox1_DropDown(object sender, EventArgs e)
+        {
+            comboBox1.Items.Clear();
+            string[] avPorts=SerialPort.GetPortNames();
+            foreach (var port in avPorts)
+            {
+                comboBox1.Items.Add(port);
+            }
+        }
+        #endregion
+
+
+        #region ComboBoxes: Select Baudrate, Select Port
+
+
+        #region Select BaudRate
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            serialPort1.BaudRate = Convert.ToInt32(comboBox2.SelectedItem);
+        }
+
+        #endregion
+
+        #region Select Port
+
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            serialPort1.PortName = Convert.ToString(comboBox1.SelectedItem);
+        }
+
+        #endregion
+
+
+        #endregion
+
+
+        #region Buttons: Write, Connect, Disconnect, Pause
+
+
+        #region Write Button
+        private void buttonWrite_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                serialPort1.WriteLine(Convert.ToString(setPoint.Value));
+                textBox1.AppendText("\r\nTX: " + Convert.ToString(setPoint.Value) + Environment.NewLine); 
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+        }
+        #endregion
+
+        #region Connect Button
+        private void buttonConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                serialPort1.Open();
+                textBox1.Text = "Connected";
+            }
+            catch (Exception ex)
+            {
+                
+                MessageBox.Show(ex.Message);
+            }
+            
+        }
+        #endregion
+
+        #region Disconnect Button
+        private void buttonDisconnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                serialPort1.Close();
+                textBox1.AppendText("\r\nDisconnected");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        #endregion
+
+        #region Clear TextBox Button
+
+        private void buttonClearText_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            chart1.Series[0].Points.Clear();
+            chart1.Series[1].Points.Clear();
+            xCounter = 0;
+        }
+        #endregion
+
+        #region Pause Button (checkBox with Appereance of a button)
+
+
+        private void checkPause_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkPause.Checked)
+            {
+                checkPause.BackColor = Color.Red;
+            }
+            else
+            {
+                checkPause.BackColor = Color.Lime;
+            }
+        }
+
+        #endregion
+
+        private void textBoxWriteData_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        #endregion
+
+    }
+}
